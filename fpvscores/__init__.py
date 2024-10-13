490c5f56df87023749d68f2338c579538b79d2cb
Aaron <23297034+Aaronsss@users.noreply.github.com>
2024-10-13 23:51:39 +0100

    Upload after save

--- fpvscores/__init__.py
+++ fpvscores/__init__.py
@@ -33,19 +33,6 @@ def register_handlers(args):
         for exporter in discover():
             args['register_fn'](exporter)
 
-
-def register_handlers_actions(args):
-    if 'register_fn' in args:
-        for effect in [
-        ActionEffect(
-            'FPV Scores Auto Upload',
-            auto_upload,
-            [
-                UIField('autoupload', "Message Text (optional)", UIFieldType.TEXT),
-            ]
-        )]:
-            args['register_fn'](effect)
-
 def initialize(rhapi):
     rhapi.fields.register_pilot_attribute( country_ui_field )
     rhapi.fields.register_pilot_attribute( UIField('safetycheck', "Safety Checked", UIFieldType.CHECKBOX) )
@@ -56,12 +43,13 @@ def initialize(rhapi):
     rhapi.ui.register_panel("fpvscores_run", "FPV Scores", "run")
 
     rhapi.fields.register_option( UIField('event_uuid', "Event UUID", UIFieldType.TEXT), 'fpvscores_format' )
+    rhapi.fields.register_option( UIField('auto_upload', "Auto Upload to FPV Scores", UIFieldType.CHECKBOX), 'fpvscores_format' )
     rhapi.ui.register_quickbutton("fpvscores_format", "fpvscores_upload", "Upload Scores to FPVScores.com", runUploadBtn, {'rhapi': rhapi})
     rhapi.ui.register_quickbutton("fpvscores_run", "fpvscores_upload_run", "Upload Scores to FPVScores.com", runUploadBtn, {'rhapi': rhapi})
     rhapi.ui.register_quickbutton("fpvscores_format", "fpvscores_clear", "Clear event data on FPVScores.com", runClearBtn, {'rhapi': rhapi})
 
     rhapi.events.on(Evt.DATA_EXPORT_INITIALIZE, register_handlers)
-    rhapi.events.on(Evt.ACTIONS_INITIALIZE, register_handlers_actions)
+    rhapi.events.on(Evt.LAPS_SAVE, auto_upload, {'rhapi': rhapi})
 
 
     bp = Blueprint(
@@ -81,8 +69,11 @@ def initialize(rhapi):
 
     rhapi.ui.blueprint_add(bp)
 
-def auto_upload(action, args):
-    print("Test Message")
+def auto_upload(args):
+    if args['rhapi'].db.option("auto_upload") == '1':
+        print("Attempting to upload to fpvscores...")
+        data = args['rhapi'].io.run_export('JSON_FPVScores_Upload')
+        uploadToFPVS(args, data, False)
 
 def write_json(data):
     payload = json.dumps(data, indent='\t', cls=AlchemyEncoder)
@@ -98,7 +89,7 @@ def runUploadBtn(args):
     args['rhapi'].ui.message_notify(args['rhapi'].__('Event data upload started.'))
     data = args['rhapi'].io.run_export('JSON_FPVScores_Upload')
     #print(data)
-    uploadToFPVS_frombtn(args, data)
+    uploadToFPVS(args, data, True)
 
 
 def runClearBtn(args):
@@ -119,7 +110,7 @@ def runClearBtn(args):
 
 
 ## FPV Scores Upload Data
-def uploadToFPVS_frombtn(args, input_data):
+def uploadToFPVS(args, input_data, button_press):
     #print('upload results to FPVScores.com')   
     json_data =  input_data['data']
     url = 'https://api.fpvscores.com/rh/0.0.3/?action=mgp_push'
@@ -132,10 +123,10 @@ def uploadToFPVS_frombtn(args, input_data):
             args['rhapi'].ui.message_notify(args['rhapi'].__('No import data found, add data (pilots, classes, heats) first.'))
         elif r.text == 'no event found':
             args['rhapi'].ui.message_notify(args['rhapi'].__('No event found - Check your event UUID on FPVScores.com.'))
-        elif r.text == 'import succesfull':
-            args['rhapi'].ui.message_notify(args['rhapi'].__('Uploaded data successfully.'))
-        else:
-            args['rhapi'].ui.message_notify(r.text)       
+        elif 'Import Succesful' in r.text:
+            if button_press:
+                args['rhapi'].ui.message_notify(args['rhapi'].__('Uploaded data successfully.'))
+        logger.info(r.text)   
     
 
 def assemble_fpvscoresUpload(rhapi):
